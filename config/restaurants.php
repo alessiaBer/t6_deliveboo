49ab996f4cf59c4dd2a4631d4867586da4519a68
alessiaBer <alexdeyn@gmail.com>
2023-06-29 16:08:21 +0200

    [seed] database

---
+++ config/restaurants.php
@@ -0,0 +1,60 @@
+<?php 
+
+return [
+    [
+        'name' => 'La Trattoria del Borgo',
+        'description' => 'Autentica cucina italiana con specialità regionali.',
+        'address' => 'Corso Vittorio Emanuele, 5',
+        'p_iva' => '01234567890',
+        'phone' => '+39 0123 456789',
+        'image_url' => 'https://wallpapercave.com/wp/wp1874174.jpg'
+    ],
+    [
+        'name' => 'Le Bistro Parisien',
+        'description' => 'Gustosi piatti francesi in un\'atmosfera accogliente.',
+        'address' => 'Via Roma, 10',
+        'p_iva' => '09876543210',
+        'phone' => '+39 9876 543210',
+        'image_url' => 'https://wallpapercave.com/wp/wp1874174.jpg'
+    ],
+    [
+        'name' => 'Mamma Mia Pizzeria',
+        'description' => 'Le migliori pizze cotte nel forno a legna secondo la tradizione napoletana.',
+        'address' => 'Via Garibaldi, 20',
+        'p_iva' => '24681357902',
+        'phone' => '+39 0246 813579',
+        'image_url' => 'https://wallpapercave.com/wp/wp1874174.jpg'
+    ],
+[
+        'name' => 'Sushiya',
+        'description' => 'Specialità giapponesi fresche e deliziose preparate al momento.',
+        'address' => 'Via Torquato Tasso',
+        'p_iva' => '75395124680',
+        'phone' => '+81 3 1234 5678',
+        'image_url' => 'https://wallpapercave.com/wp/wp1874174.jpg'
+    ],
+    [
+        'name' => 'El Rancho Steakhouse',
+        'description' => 'Tagli di carne succulenti e gustosi in un ambiente rustico.',
+        'address' => 'Via dei Mercanti, 15',
+        'p_iva' => '57904681324',
+        'phone' => '+34 912 345 678',
+        'image_url' => 'https://wallpapercave.com/wp/wp1874174.jpg'
+    ],
+    [
+        'name' => 'Cantina Mexicana',
+        'description' => 'Piatti autentici messicani con una vasta selezione di tequila.',
+        'address' => 'Via dei Principati, 8',
+        'p_iva' => '96123456780',
+        'phone' => '+52 55 6789 1234',
+        'image_url' => 'https://wallpapercave.com/wp/wp1874174.jpg'
+    ],
+    [
+        'name' => 'Thai Spice',
+        'description' => 'Esperienza culinaria thailandese con sapori autentici.',
+        'address' => 'Via dei Canali, 23',
+        'p_iva' => '46801357924',
+        'phone' => '+66 2 3456 7890',
+        'image_url' => 'https://wallpapercave.com/wp/wp1874174.jpg'
+    ]
+];
--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Restaurant;
+use Illuminate\Support\Str;
 
 class RestaurantSeeder extends Seeder
 {
@@ -14,6 +16,18 @@ class RestaurantSeeder extends Seeder
      */
     public function run()
     {
-        
+        $restaurants = config('restaurants');
+
+        foreach($restaurants as $restaurant) {
+            $newRestaurant = new Restaurant();
+            $newRestaurant->name = $restaurant['name'];
+            $newRestaurant->slug = Str::slug($restaurant['name']);
+            $newRestaurant->description = $restaurant['description'];
+            $newRestaurant->address = $restaurant['address'];
+            $newRestaurant->p_iva = $restaurant['p_iva'];
+            $newRestaurant->phone = $restaurant['phone'];
+            $newRestaurant->image_url = $restaurant['image_url'];
+            $newRestaurant->save();
+        }
     }
 }
