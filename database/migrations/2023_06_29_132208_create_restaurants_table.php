5e75c2326ba2b9232ee763130ca7655379347c29
alessiaBer <alexdeyn@gmail.com>
2023-06-29 15:32:16 +0200

    [add] migration restaurants table

---
+++ database/migrations/2023_06_29_132208_create_restaurants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->text('description');
+            $table->string('address', 255);
+            $table->string('p_iva', 15);
+            $table->string('phone', 20);
+            $table->string('image_url', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
