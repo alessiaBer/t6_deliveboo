66db64b92bb20eed093fe989cd59c72d3348590b
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-06-30 11:22:13 +0200

    Add relationship between models

--- app/Models/Plate.php
+++ app/Models/Plate.php
@@ -2,13 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Plate extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'slug', 'description', 'image_url', 'price', 'is_available', 'restaurant_id'];
+
+    public static function generateSlug($name){
+        return Str::slug($name, "-");
+    }
+
+    /**
+     * Get the user that owns the Plate
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function restaurant(): BelongsTo
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
 
 }
