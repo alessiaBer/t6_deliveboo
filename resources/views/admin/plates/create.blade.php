b6d5f669c2ab76bbc22928b75eb895012c4ca484
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-07-06 12:28:46 +0200

    add client-side validations for plates

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -9,17 +9,18 @@
         @endforeach
     @endif
 
-    <form action="{{ route('admin.plates.store') }}" method="post" enctype="multipart/form-data">
+    <form action="{{ route('admin.plates.store') }}" method="post" id="validation" enctype="multipart/form-data"
+        class="needs-validation">
         @csrf
 
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror"
                 value="{{ old('name') }}" placeholder="plate name">
-
-            @error('name')
+            <div class="invalid-feedback fw-bold"></div>
+            {{-- @error('name')
                 <small class="text-danger">Please, fill the field correctly</small>
-            @enderror
+            @enderror --}}
         </div>
 
         <div class="mb-3">
@@ -27,10 +28,11 @@
             <input type="text" name="description" id="description"
                 class="form-control @error('description') is-invalid @enderror" value="{{ old('description') }}"
                 placeholder="plate description">
-
+            <div class="invalid-feedback fw-bold"></div>
+            {{-- 
             @error('description')
                 <small class="text-danger">Please, fill the field correctly</small>
-            @enderror
+            @enderror --}}
 
         </div>
 
@@ -39,35 +41,123 @@
             <input type="file" name="image_url" id="image_url"
                 class="form-control @error('image_url') is-invalid @enderror" value="{{ old('image_url') }}"
                 placeholder="plate image_url">
+            <div class="invalid-feedback fw-bold"></div>
 
-            @error('image_url')
-                <small class="text-danger">Please, fill the field correctly</small>
-            @enderror
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">price</label>
-            <input type="text" name="price" id="price" class="form-control @error('price') is-invalid @enderror"
+            <input type="number" name="price" id="price" class="form-control @error('price') is-invalid @enderror"
                 value="{{ old('price') }}" placeholder="plate price">
+            <div class="invalid-feedback fw-bold"></div>
 
-            @error('price')
-                <small class="text-danger">Please, fill the field correctly</small>
-            @enderror
         </div>
 
         <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="1" id="is_available"
-                name="is_available">
+            <input class="form-check-input" type="checkbox" value="1" id="is_available" name="is_available">
             <label class="form-check-label text-white" for="is_available">
                 Is Available?
             </label>
-            @error('is_available')
-                <small class="text-danger">Please, fill the field correctly</small>
-            @enderror
+
         </div>
 
         <a class="btn btn-outline-light" href="{{ route('admin.plates.index') }}" role="button">Back</a>
         <button type="submit" class="btn btn-primary">Insert plate</button>
         <button type="reset" class="btn btn-danger">Reset fields</button>
     </form>
+
+    <script>
+        const form = document.getElementById('validation');
+        const nameInput = document.getElementById('name');
+        const imageInput = document.getElementById('image_url');
+        const descriptionInput = document.getElementById('description');
+        const priceInput = document.getElementById('price');
+
+        const inputs = [nameInput, imageInput, descriptionInput, priceInput];
+
+        form.addEventListener('submit', function(event) {
+            let isValidForm = true;
+
+            inputs.forEach(input => {
+                if (!isValidInput(input)) {
+                    event.preventDefault();
+                    isValidForm = false;
+                } else {
+                    input.nextElementSibling.textContent = '';
+                }
+            });
+        });
+
+        inputs.forEach(input => {
+            input.addEventListener('input', function() {
+                if (isValidInput(input)) {
+                    input.classList.remove('is-invalid');
+                    input.classList.add('is-valid');
+                    input.nextElementSibling.textContent = '';
+                } else {
+                    input.classList.add('is-invalid');
+                    input.classList.remove('is-valid');
+                    input.nextElementSibling.textContent = getErrorMessage(input);
+                }
+            });
+        });
+
+        function isValidInput(input) {
+            if (input === nameInput) {
+                return isValidName(nameInput.value);
+            } else if (input === imageInput) {
+                return isValidImage(imageInput.value);
+            } else if (input === descriptionInput) {
+                return isValidDescription(descriptionInput.value);
+            } else if (input === priceInput) {
+                return isValidPrice(priceInput.value);
+            }
+
+            return true;
+        }
+
+        function getErrorMessage(input) {
+            if (input === nameInput) {
+                return 'Name must have at least 3 letters';
+            } else if (input === imageInput) {
+                return 'Invalid image format';
+            } else if (input === descriptionInput) {
+                return 'Description must have at least 6 letters';
+            } else if (input === priceInput) {
+                return 'Price must have at least 2 digits';
+            }
+            return '';
+        }
+
+        function isValidName(nameInput) {
+            return nameInput.trim().length >= 3;
+        }
+
+        function isValidAddress(addressInput) {
+            return addressInput.trim().length >= 6;
+        }
+
+
+        function isValidImage(imageInput) {
+            const formats = ['.png', '.jpeg', '.jpg', '.gif', '.svg'];
+
+            for (let i = 0; i < formats.length; i++) {
+                const format = formats[i];
+                //console.log(format);
+                if (imageInput.includes(format)) {
+                    return true;
+                }
+            }
+            return false
+        }
+
+        function isValidDescription(descriptionInput) {
+            return descriptionInput.trim().length >= 6;
+        }
+
+        function isValidPrice(priceInput) {
+            const priceRegex = /^[0-9]+\.[0-9]{2}$/;
+            return priceRegex.test(priceInput);
+        }
+    </script>
 @endsection
--- resources/views/admin/plates/edit.blade.php
+++ resources/views/admin/plates/edit.blade.php
@@ -13,7 +13,8 @@
         @endif
         <h2 class="text-center text-dark text-uppercase">you are currently editing the item #{{ $plate->id }}</h2>
         <h2 class="text-center text-dark text-uppercase">{{ $plate->name }}</h2>
-        <form action="{{ route('admin.plates.update', $plate) }}" method="post" class="text-light bg-dark rounded p-5" enctype="multipart/form-data">
+        <form action="{{ route('admin.plates.update', $plate) }}" id="validation" method="post"
+            class="text-light bg-dark rounded p-5 needs-validation" enctype="multipart/form-data">
             @csrf
             @method('PUT')
 
@@ -22,6 +23,7 @@
                 <label for="name" class="form-label text-uppercase">name</label>
                 <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror"
                     placeholder="only name there" aria-describedby="nameHelper" value="{{ old('name', $plate->name) }}">
+                <div class="invalid-feedback fw-bold"></div>
                 <small id="nameHelper" class="text-muted text-uppercase">insert the name</small>
                 @error('name')
                     <div class="alert alert-danger p-3 m-3" role="alert">
@@ -35,6 +37,7 @@
                 <input type="text" name="description" id="description"
                     class="form-control @error('description') is-invalid @enderror" placeholder="only description there"
                     aria-describedby="nameHelper" value="{{ old('description', $plate->description) }}">
+                <div class="invalid-feedback fw-bold"></div>
                 <small id="nameHelper" class="text-muted text-uppercase">insert the description</small>
                 @error('name')
                     <div class="alert alert-danger p-3 m-3" role="alert">
@@ -48,6 +51,7 @@
                 <input type="text" name="price" id="price"
                     class="form-control @error('price') is-invalid @enderror" placeholder="only price there"
                     aria-describedby="nameHelper" value="{{ old('price', $plate->price) }}">
+                <div class="invalid-feedback fw-bold"></div>
                 <small id="nameHelper" class="text-muted text-uppercase">insert the price</small>
                 @error('name')
                     <div class="alert alert-danger p-3 m-3" role="alert">
@@ -57,7 +61,8 @@
             </div>
 
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="{{old('is_available', $plate->is_available)}}" {{$plate->is_available ? 'checked' : ''}} id="is_available" name="is_available">
+                <input class="form-check-input" type="checkbox" value="{{ old('is_available', $plate->is_available) }}"
+                    {{ $plate->is_available ? 'checked' : '' }} id="is_available" name="is_available">
                 <label class="form-check-label text-white" for="is_available">
                     Is Available?
                 </label>
@@ -67,7 +72,7 @@
             </div>
 
             <div class="d-flex">
-                <img width="100" src="{{ asset('storage/' . $plate->image_url)}}" alt="{{$plate->image_url}}">
+                <img width="100" src="{{ asset('storage/' . $plate->image_url) }}" alt="{{ $plate->image_url }}">
             </div>
 
             <div class="mb-3">
@@ -75,7 +80,7 @@
                 <input type="file" name="image_url" id="image_url"
                     class="form-control @error('image_url') is-invalid @enderror" value="{{ old('image_url') }}"
                     placeholder="plate image_url">
-    
+                <div class="invalid-feedback fw-bold"></div>
                 @error('image_url')
                     <small class="text-danger">Please, fill the field correctly</small>
                 @enderror
@@ -89,4 +94,100 @@
             </div>
         </form>
     </div>
+
+
+    <script>
+        const form = document.getElementById('validation');
+        const nameInput = document.getElementById('name');
+        const priceInput = document.getElementById('price');
+        const imageInput = document.getElementById('image_url');
+        const descriptionInput = document.getElementById('description');
+
+        const inputs = [nameInput, imageInput, descriptionInput, priceInput];
+
+        form.addEventListener('submit', function(event) {
+            let isValidForm = true;
+
+            inputs.forEach(input => {
+                if (!isValidInput(input)) {
+                    event.preventDefault();
+                    isValidForm = false;
+                } else {
+                    input.nextElementSibling.textContent = '';
+                }
+            });
+        });
+
+        inputs.forEach(input => {
+            input.addEventListener('input', function() {
+                if (isValidInput(input)) {
+                    input.classList.remove('is-invalid');
+                    input.classList.add('is-valid');
+                    input.nextElementSibling.textContent = '';
+                } else {
+                    input.classList.add('is-invalid');
+                    input.classList.remove('is-valid');
+                    input.nextElementSibling.textContent = getErrorMessage(input);
+                }
+            });
+        });
+
+        function isValidInput(input) {
+            if (input === nameInput) {
+                return isValidName(nameInput.value);
+            } else if (input === imageInput) {
+                return isValidImage(imageInput.value);
+            } else if (input === descriptionInput) {
+                return isValidDescription(descriptionInput.value);
+            } else if (input === priceInput) {
+                return isValidPrice(priceInput.value);
+            }
+
+            return true;
+        }
+
+        function getErrorMessage(input) {
+            if (input === nameInput) {
+                return 'Name must have at least 3 letters';
+            } else if (input === imageInput) {
+                return 'Invalid image format';
+            } else if (input === descriptionInput) {
+                return 'Description must have at least 6 letters';
+            } else if (input === priceInput) {
+                return 'Price must have at least 2 digits';
+            }
+            return '';
+        }
+
+        function isValidName(nameInput) {
+            return nameInput.trim().length >= 3;
+        }
+
+        function isValidAddress(addressInput) {
+            return addressInput.trim().length >= 6;
+        }
+
+
+        function isValidImage(imageInput) {
+            const formats = ['.png', '.jpeg', '.jpg', '.gif', '.svg'];
+
+            for (let i = 0; i < formats.length; i++) {
+                const format = formats[i];
+                //console.log(format);
+                if (imageInput.includes(format)) {
+                    return true;
+                }
+            }
+            return false
+        }
+
+        function isValidDescription(descriptionInput) {
+            return descriptionInput.trim().length >= 6;
+        }
+
+        function isValidPrice(priceInput) {
+            const priceRegex = /^[0-9]+\.[0-9]{2}$/;
+            return priceRegex.test(priceInput);
+        }
+    </script>
 @endsection
