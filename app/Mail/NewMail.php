86fd9ca755e50af65bdc5c911e1f6bbd33551dc3
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-07-11 12:51:13 +0200

    ADD email funzionante

---
+++ app/Mail/NewMail.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $lead;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($lead)
+    {
+        $this->lead = $lead;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'New Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.lead.new',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
